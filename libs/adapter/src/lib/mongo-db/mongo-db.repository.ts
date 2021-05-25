fd336cd57440137cbd6f38c4de38e9b330a2ebd4
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-25 14:14:04 -0500

    MongoDB repository

---
+++ libs/adapter/src/lib/mongo-db/mongo-db.repository.ts
@@ -0,0 +1,16 @@
+import { Collection } from 'mongodb';
+
+export class MongoDBRepository<T extends { id: string }> {
+  constructor(private readonly collection: Collection) {}
+
+  async getById(id: string): Promise<T> {
+    const filterQuery = { _id: id };
+    return this.collection.findOne(filterQuery);
+  }
+
+  async save(entity: T): Promise<void> {
+    const filterQuery = { _id: entity.id };
+    const options = { upsert: true };
+    await this.collection.findOneAndReplace(filterQuery, entity, options);
+  }
+}
