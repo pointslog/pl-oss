0eaa3c4c8fab58040f4baa115e8da03b4dfb3480
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-25 10:11:00 -0500

    Adapters library (#5)
    * Base branch
    
    * Bootstraped
    
    * Prepare workflow done
    
    * Name added
    
    * Project added
    
    * All done
    
    * Babel improved
    
    * Code copied
    
    * File stolen
    
    * Boilerplate code removed
    
    * Aggregate tested
    
    * File created
    
    * Lowercased
    
    * Domain core aggregate repository test (#4)
    
    * Dummy objects created
    
    * Work in progress - 1 test is failing
    
    * GetById test fixed
    
    * AggregateRepository -> save() - test done
    
    * Unused code removed
    
    * Requested changes added
    
    * Adapter lib created
    
    * EventStoreDBEventSubscription done
    
    * Moved to adapter
    
    * evets.length used
    
    * Nth called used
    
    Co-authored-by: Ronak Mistry <ronakmistry36@gmail.com>

---
+++ libs/adapter/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/event-store-db/event-store-db.event-subscription';
---
+++ libs/adapter/src/lib/event-store-db/event-store-db.event-subscription.ts
@@ -0,0 +1,17 @@
+import { EventStoreDBClient, streamNameFilter } from '@eventstore/db-client';
+import { EventListener, EventSubscription, Event } from '@pl-oss/domain-core';
+
+export class EventStoreDBEventSubscription implements EventSubscription {
+  constructor(private readonly client: EventStoreDBClient) {}
+
+  async register(listener: EventListener): Promise<void> {
+    const prefixes = listener.eventTypePrefixes;
+    const filter = streamNameFilter({ prefixes });
+    const subscription = this.client.subscribeToAll({ filter });
+
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const { event } of subscription) {
+      await listener.on(event as unknown as Event);
+    }
+  }
+}
