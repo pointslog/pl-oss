f70d55d3ca263d79266b93e7d91185d4cbdd21ed
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-26 14:33:04 -0500

    Vue Support (#9)
    * Vue Plugin added
    
    * Vue Core created
    
    * Tested with Pug

---
+++ libs/vue-core/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'vue-core',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/libs/vue-core',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+    'vue-jest': {
+      tsconfig: `${__dirname}/tsconfig.spec.json`,
+    },
+  },
+};
