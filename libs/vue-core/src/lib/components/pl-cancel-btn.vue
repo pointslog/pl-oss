d9f289235796b3999bfdc42dc12f645efa474917
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-26 16:14:49 -0500

    Done and Cancel Btn

---
+++ libs/vue-core/src/lib/components/pl-cancel-btn.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  v-btn(
+    text
+    v-bind="$attrs"
+    v-on="$listeners"
+  ) {{ 'Cancel' }}
+</template>
---
+++ libs/vue-core/src/lib/components/pl-done-btn.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  v-btn(
+    color="primary"
+    text
+    type="submit"
+    v-bind="$attrs"
+    v-on="$listeners"
+  ) {{ 'Done' }}
+</template>
