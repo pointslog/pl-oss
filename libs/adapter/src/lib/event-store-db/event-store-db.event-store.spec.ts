2b9b732fbbfb7f7037cac6b72025f47bd4d5e2fe
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-28 02:15:23 -0500

    EventStoreDB EventStore (#8)
    * MongoDB repository
    
    * Code cleaned
    
    * Test fixed
    
    * Test fixed
    
    * Test fixed
    
    * EventStoreDBEventStore added
    
    * Spacing

---
+++ libs/adapter/src/lib/event-store-db/event-store-db.event-store.spec.ts
@@ -0,0 +1,94 @@
+import {
+  EventStoreDBClient,
+  FORWARDS,
+  JSONEventData,
+  NO_STREAM,
+  ResolvedEvent,
+  START,
+} from '@eventstore/db-client';
+import { Event as DomainEvent } from '@pl-oss/domain-core';
+import { EventStoreDBEventStore } from './event-store-db.event-store';
+
+jest.mock('@eventstore/db-client', () => ({
+  ...(jest.requireActual('@eventstore/db-client')),
+  EventStoreDBClient: {
+    connectionString: () => ({
+      appendToStream: jest.fn(),
+      readStream: jest.fn(),
+    }),
+  },
+}));
+
+describe('EventStoreDBEventStore', () => {
+  const id = 'id';
+  const domainEvents: DomainEvent[] = [];
+  const jsonEvents: JSONEventData[] = [];
+  const resolvedEvents: ResolvedEvent[] = [];
+  const stream = `TestAggregate-${id}`;
+
+  let testEventStoreDBClient: EventStoreDBClient;
+  let testEventStoreDBEventStore: EventStoreDBEventStore;
+
+  beforeEach(() => {
+    testEventStoreDBClient = EventStoreDBClient.connectionString('');
+    testEventStoreDBEventStore = new EventStoreDBEventStore(testEventStoreDBClient);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('determineExpectedRevision', () => {
+    it('should return NO_STREAM if revision is -1', async () => {
+      const revision = -1;
+      const expectedRevision = EventStoreDBEventStore.determineExpectedRevision(revision);
+      expect(expectedRevision).toStrictEqual(NO_STREAM);
+    });
+
+    it('should return BigInt if revision is anything but -1', async () => {
+      const revision = 1;
+      const expectedRevision = EventStoreDBEventStore.determineExpectedRevision(revision);
+      expect(expectedRevision).toStrictEqual(BigInt(revision));
+    });
+  });
+
+  describe('mapEventToJsonEvent', () => {
+    it('should map domain event to json event', async () => {
+      const mappedJsonEvents = domainEvents.map(EventStoreDBEventStore.mapEventToJsonEvent);
+      expect(mappedJsonEvents).toStrictEqual(jsonEvents);
+    });
+  });
+
+  describe('mapResolvedEventToEvent', () => {
+    it('should map resolved event to domain event', async () => {
+      const mappedDomainEvents = resolvedEvents.map(EventStoreDBEventStore.mapResolvedEventToEvent);
+      expect(mappedDomainEvents).toStrictEqual(domainEvents);
+    });
+  });
+
+  describe('append', () => {
+    it('should call appendToStream', async () => {
+      await testEventStoreDBEventStore.append(stream, domainEvents, -1);
+
+      expect(testEventStoreDBClient.appendToStream).toHaveBeenCalledTimes(1);
+      expect(testEventStoreDBClient.appendToStream)
+        .toHaveBeenNthCalledWith(1, stream, jsonEvents, { expectedRevision: NO_STREAM });
+    });
+  });
+
+  describe('read', () => {
+    it('should call readStream', async () => {
+      const direction = FORWARDS;
+      const fromRevision = START;
+
+      jest.spyOn(testEventStoreDBClient, 'readStream').mockResolvedValue(resolvedEvents);
+
+      const events = await testEventStoreDBEventStore.read(stream);
+
+      expect(events).toMatchObject(domainEvents);
+      expect(testEventStoreDBClient.readStream).toHaveBeenCalledTimes(1);
+      expect(testEventStoreDBClient.readStream)
+        .toHaveBeenNthCalledWith(1, stream, { direction, fromRevision });
+    });
+  });
+});
---
+++ libs/adapter/src/lib/event-store-db/event-store-db.event-store.ts
@@ -0,0 +1,43 @@
+import {
+  EventStoreDBClient,
+  FORWARDS,
+  jsonEvent,
+  JSONEventData,
+  NO_STREAM,
+  ResolvedEvent,
+  START,
+} from '@eventstore/db-client';
+import { EventStore, Event as DomainEvent } from '@pl-oss/domain-core';
+
+export class EventStoreDBEventStore implements EventStore {
+  constructor(private readonly eventStoreDBClient: EventStoreDBClient) {}
+
+  static determineExpectedRevision(revision: number): typeof NO_STREAM | bigint {
+    return revision === -1 ? NO_STREAM : BigInt(revision);
+  }
+
+  static mapEventToJsonEvent(event: DomainEvent): JSONEventData {
+    const type = event.constructor.name;
+    const { data, metadata } = event;
+    return jsonEvent({ type, data, metadata });
+  }
+
+  static mapResolvedEventToEvent(event: ResolvedEvent): DomainEvent {
+    return event.event as unknown as DomainEvent;
+  }
+
+  async append(stream: string, events: DomainEvent[], expectedRevision: number): Promise<void> {
+    const jsonEvents = events.map(EventStoreDBEventStore.mapEventToJsonEvent);
+    const determinedRevision = EventStoreDBEventStore.determineExpectedRevision(expectedRevision);
+    await this.eventStoreDBClient
+      .appendToStream(stream, jsonEvents, { expectedRevision: determinedRevision });
+  }
+
+  async read(stream: string): Promise<DomainEvent[]> {
+    const direction = FORWARDS;
+    const fromRevision = START;
+    const resolvedEvents = await this.eventStoreDBClient
+      .readStream(stream, { direction, fromRevision });
+    return resolvedEvents.map(EventStoreDBEventStore.mapResolvedEventToEvent);
+  }
+}
