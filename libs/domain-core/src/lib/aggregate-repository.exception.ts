2545bf130808887f152504ffcfa04e62d9e12c69
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-06-05 00:12:04 -0500

    Removed unused repos

--- libs/domain-core/src/lib/aggregate-repository.exception.ts
+++
@@ -1,6 +0,0 @@
-export class AggregateRepositoryException extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'AggregateRepositoryException';
-  }
-}
--- libs/domain-core/src/lib/entity-repository.exception.ts
+++
@@ -1,6 +0,0 @@
-export class EntityRepositoryException extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'EntityRepositoryException';
-  }
-}
