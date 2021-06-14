fecc268ee06bb3f7af318305da08180523a660f7
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-13 22:42:38 -0500

    getAll Added

--- libs/domain-core/src/lib/entity-repository.ts
+++ libs/domain-core/src/lib/entity-repository.ts
@@ -1,6 +1,7 @@
 import { Entity } from './entity';
 
 export interface EntityRepository<T extends Entity> {
+  getAll(): Promise<T[]>
   getById(id: string): Promise<T>
   save(entity: T): Promise<void>
 }
