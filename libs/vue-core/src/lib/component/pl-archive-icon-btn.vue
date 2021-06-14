be9732463bca71aa64c630217cb4439fa32c767a
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-14 12:54:51 -0500

    New components

---
+++ libs/vue-core/src/lib/component/pl-archive-icon-btn.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  v-btn(
+    icon
+    v-bind="$attrs"
+    v-on="$listeners"
+  )
+    v-icon {{ 'mdi-archive' }}
+</template>
--- libs/vue-core/src/lib/plugins/pl-components.plugin.ts
+++ libs/vue-core/src/lib/plugins/pl-components.plugin.ts
@@ -1,15 +1,21 @@
 import { VueConstructor } from 'vue/types/umd';
-import PLCancelBtn from '../components/pl-cancel-btn.vue';
-import PLDialog from '../components/pl-dialog.vue';
-import PLDoneBtn from '../components/pl-done-btn.vue';
-import PLMaxWidth from '../components/pl-max-width.vue';
-import PLTextField from '../components/pl-text-field.vue';
+import PLArchiveIconBtn from '../component/pl-archive-icon-btn.vue';
+import PLCancelBtn from '../component/pl-cancel-btn.vue';
+import PLDialog from '../component/pl-dialog.vue';
+import PLDoneBtn from '../component/pl-done-btn.vue';
+import PLEditIconBtn from '../component/pl-edit-icon-btn.vue';
+import PLMaxWidth from '../component/pl-max-width.vue';
+import PLPlusIconBtn from '../component/pl-plus-icon-btn.vue';
+import PLTextField from '../component/pl-text-field.vue';
 
 function install(Vue: VueConstructor): void {
+  Vue.component('pl-archive-icon-btn', PLArchiveIconBtn);
   Vue.component('pl-cancel-btn', PLCancelBtn);
   Vue.component('pl-dialog', PLDialog);
   Vue.component('pl-done-btn', PLDoneBtn);
+  Vue.component('pl-edit-icon-btn', PLEditIconBtn);
   Vue.component('pl-max-width', PLMaxWidth);
+  Vue.component('pl-plus-icon-btn', PLPlusIconBtn);
   Vue.component('pl-text-field', PLTextField);
 }
 
