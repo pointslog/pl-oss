05acb1dbf5283d8c84511cac343b4fd9b1bc96f3
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-06-20 00:24:50 -0500

    Create PLTextComponent

---
+++ libs/vue-core/src/lib/component/pl-text-form.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  v-form(@submit.prevent="formSubmitHandler")
+    v-card
+      v-card-title {{ title }}
+
+      v-card-text
+        pl-text-field(
+          autofocus
+          v-bind="$attrs"
+          v-on="$listeners"
+        )
+
+      v-card-actions
+        v-spacer
+        pl-cancel-btn(@click="cancelBtnClickHandler")
+        pl-done-btn(:loading="loading")
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+const TextFormProps = Vue.extend({
+  props: {
+    loading: { type: Boolean },
+    title: { type: String, required: true },
+  },
+});
+
+@Component
+export default class PLTextForm extends TextFormProps {
+  formSubmitHandler() {
+    this.$emit('submit');
+  }
+
+  cancelBtnClickHandler() {
+    this.$emit('cancel');
+  }
+}
+</script>
