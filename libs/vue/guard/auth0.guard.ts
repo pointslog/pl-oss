205e36e7f05543c628ff66e6a2ff9c4f91188968
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-22 22:17:57 -0500

    Patch auth0 (#62)
    * AppContext adde
    
    * AppContext added
    
    * If updated
    
    * Fixed

--- libs/vue/guard/auth0.guard.ts
+++ libs/vue/guard/auth0.guard.ts
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import { NavigationGuardNext, Route } from 'vue-router';
 import { getInstance } from '../plugin/auth0.plugin';
 
-export function Auth0Guard(to: Route, _: Route, next: NavigationGuardNext<Vue>): void {
+export function Auth0Guard(
+  to: Route,
+  _: Route,
+  next: NavigationGuardNext<Vue>,
+): void {
   const authService = getInstance();
 
   const fn = () => {
