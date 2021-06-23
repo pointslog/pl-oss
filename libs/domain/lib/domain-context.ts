69f76cee7e43a97a7945dc801473dde7d2e2206f
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-22 21:29:00 -0500

    Add AppContext (#60)
    * AppContext adde
    
    * AppContext added

--- libs/domain/lib/domain-context.ts
+++ libs/domain/lib/domain-context.ts
@@ -1,15 +1,9 @@
 import { CommandHandler } from './command-handler';
 import { Command } from './command';
-import { EntityRepository } from './entity-repository';
-import { Entity } from './entity';
 import { QueryHandler } from './query-handler';
 import { Query } from './query';
-import { AggregateRepository } from './aggregate-repository';
-import { Aggregate } from './aggregate';
 
 export interface DomainContext {
-  readonly aggregateRepositoryMap: Record<string, AggregateRepository<Aggregate>>
   readonly commandHandlerMap: Record<string, CommandHandler<Command>>
-  readonly entityRepositoryMap: Record<string, EntityRepository<Entity>>
   readonly queryHandlerMap: Record<string, QueryHandler<Query>>
 }
