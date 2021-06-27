28ec4900e03032f3127973f2f7296347e251fab6
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-26 20:51:23 -0500

    refactor: rearranged

--- libs/adapter/nest/message/message.module.ts
+++ libs/adapter/nest/message/message.module.ts
@@ -2,10 +2,7 @@ import { DynamicModule, Logger, Module } from '@nestjs/common';
 import { HandlerMap } from '@pl-oss/domain';
 import { MessageController } from './message.controller';
 
-@Module({
-  controllers: [MessageController],
-  providers: [Logger],
-})
+@Module({ controllers: [MessageController], providers: [Logger] })
 export class MessageModule {
   static register(handlerMap: HandlerMap): DynamicModule {
     const providers = [{ provide: 'HandlerMap', useValue: handlerMap }];
