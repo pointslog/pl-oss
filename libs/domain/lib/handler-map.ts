23d174470c9fbafcc60107cd866f0012b37237d8
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-26 19:21:26 -0500

    refactor: HandlerMap added

---
+++ libs/domain/lib/handler-map.ts
@@ -0,0 +1,6 @@
+import { CommandHandler } from './command-handler';
+import { QueryHandler } from './query-handler';
+
+type Handler = CommandHandler | QueryHandler;
+
+export type HandlerMap = Record<string, Handler>;
