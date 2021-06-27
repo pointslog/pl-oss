56cc6d386eef114503268aa6e71c579e87848048
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-06-26 19:43:08 -0500

    Update libs/domain/lib/handler-map.ts
    Co-authored-by: Shailendra Shukla <shailendrashuklapro@gmail.com>

--- libs/domain/lib/handler-map.ts
+++ libs/domain/lib/handler-map.ts
@@ -2,5 +2,4 @@ import { CommandHandler } from './command-handler';
 import { QueryHandler } from './query-handler';
 
 type Handler = CommandHandler | QueryHandler;
-
 export type HandlerMap = Record<string, Handler>;
