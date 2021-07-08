c622c2c4f50f40e4c3c9058b8ce22c4ccdf5d9c0
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-07-07 20:03:21 -0500

    feat: add slot to pl-text-form and add graphql pubsub in context

--- libs/domain/lib/context.ts
+++ libs/domain/lib/context.ts
@@ -1,7 +1,9 @@
+import { PubSub } from 'graphql-subscriptions';
 import { FeatureService } from './feature-service';
 import { LoggingService } from './logging-service';
 
 export interface Context {
-  featureService?: FeatureService
+  featureService?: FeatureService,
+  graphQLPubSub?: PubSub,
   loggingService?: LoggingService,
 }
