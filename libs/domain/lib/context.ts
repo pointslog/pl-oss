c1f270238ef5c0f64946ba2fdf9e8d035b883e1b
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-07-28 18:39:02 -0500

    refactor(file-context): remove file-context

--- libs/domain/lib/context.ts
+++ libs/domain/lib/context.ts
@@ -1,6 +1,6 @@
-import { FileStore } from '@pl-oss/file-context';
 import { EventStore } from './event-store';
 import { FeatureService } from './feature-service';
+import { FileStore } from './file-store';
 import { GraphQLService } from './graphql.service';
 import { LoggingService } from './logging-service';
 
--- libs/file-context/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/file.entity';
-export * from './lib/file-store';
-export * from './lib/id-should-be-unique.exception';
-export * from './lib/mongo-db.file-store';
