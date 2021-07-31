4c9585838809655b9f9681b615cf837084215972
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-07-30 20:02:05 -0500

    network exception

--- libs/adapter/file-store/uri.file-store.ts
+++ libs/adapter/file-store/uri.file-store.ts
@@ -1,5 +1,6 @@
 import { FileEntity, FileStore, UnimplementedException } from '@pl-oss/domain';
 import fetch from 'node-fetch';
+import { NetworkException } from './network.exception';
 
 export class URIFileStore implements FileStore {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars,class-methods-use-this
@@ -10,7 +11,7 @@ export class URIFileStore implements FileStore {
   // eslint-disable-next-line class-methods-use-this
   async read(id: string): Promise<FileEntity> {
     const response = await fetch(id);
-    if (response.status !== 200) throw new Error('network');
+    if (response.status !== 200) throw new NetworkException();
     const buffer = await response.buffer();
     const contentType = await response.headers.get('content-type');
     const base64Data = `data:${contentType};base64,${buffer.toString('base64')}`;
