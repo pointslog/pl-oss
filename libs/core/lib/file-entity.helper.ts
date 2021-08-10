0968845d0f0e9c749ae318dd52d74de84adbb973
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-08-09 22:31:30 -0500

    feat: determine extension improved

--- libs/core/lib/file-entity.helper.ts
+++ libs/core/lib/file-entity.helper.ts
@@ -1,10 +1,30 @@
 import { FileEntity } from './file.entity';
+import { InvalidFileExtensionException } from './invalid-file-extension.exception';
+
+const ALLOWED_EXTENSIONS = [
+  'gif',
+  'jpeg',
+  'jpg',
+  'mov',
+  'mp3',
+  'mp4',
+  'pdf',
+  'png',
+];
+
+function isValidExtension(extension: string): boolean {
+  return ALLOWED_EXTENSIONS.includes(extension);
+}
 
 export class FileEntityHelper {
   constructor(private readonly fileEntity: FileEntity) {}
 
   get extension(): string {
-    return this.mimeType.split('/')[1];
+    const fromId = this.fileEntity.id.split('.').pop().toLowerCase();
+    if (isValidExtension(fromId)) return fromId;
+    const fromFile = this.mimeType.split('/')[1];
+    if (isValidExtension(fromFile)) return fromFile;
+    throw new InvalidFileExtensionException();
   }
 
   get mimeType(): string {
