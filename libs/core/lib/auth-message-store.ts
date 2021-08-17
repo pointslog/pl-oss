378e753b3e209fa02babdab54591a907f2be91c6
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-08-16 19:19:43 -0500

    refactor: add auth-message-store

--- libs/core/lib/auth-message-store.ts
+++ libs/core/lib/auth-message-store.ts
@@ -1,7 +1,7 @@
 export type AuthMessage = Record<string, string>;
 
 export interface AuthMessageStore {
-  delete(walletAddress: string): void
-  getByWalletAddress(walletAddress: string): string
-  save(walletAddress: string, message: string): void
+  delete(walletAddress: string): void;
+  getByWalletAddress(walletAddress: string): string;
+  save(walletAddress: string, message: string): void;
 }
--- libs/core/lib/context.ts
+++ libs/core/lib/context.ts
@@ -1,3 +1,4 @@
+import { AuthMessageStore } from './auth-message-store';
 import { Environment } from './environment';
 import { EventStore } from './event-store';
 import { FeatureService } from './feature-service';
@@ -6,6 +7,7 @@ import { GraphQLService } from './graphql-service';
 import { LoggingService } from './logging-service';
 
 export interface Context {
+  authMessageStore?: AuthMessageStore;
   environment?: Environment;
   eventStore?: EventStore;
   featureService?: FeatureService;
