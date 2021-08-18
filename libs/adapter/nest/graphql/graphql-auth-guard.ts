ef4b20d10397cdcef77404ec30a310d8782f9dff
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-08-17 19:04:29 -0500

    feat: add graphql-auth-guard

---
+++ libs/adapter/nest/graphql/graphql-auth-guard.ts
@@ -0,0 +1,14 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class GraphQLJWTAuthGuard extends AuthGuard('jwt') {
+  // eslint-disable-next-line class-methods-use-this
+  getRequest(context: ExecutionContext) {
+    return GqlExecutionContext
+      .create(context)
+      .getContext()
+      .req;
+  }
+}
