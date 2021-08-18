9b99e192b293d2cb0d34b4ef150ae4f75fb4448c
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-08-18 01:27:56 -0500

    refactor: public-key-challenge-store (#151)
    * refactor: in-memory-public-key-challenge-store
    
    * chore: version update
    
    * chore: version update

--- libs/core/lib/public-key-challenge-store.ts
+++ libs/core/lib/public-key-challenge-store.ts
@@ -3,7 +3,6 @@ export type Challenge = string;
 export type PublicKeyChallengeMap = Record<PublicKey, Challenge>;
 
 export interface PublicKeyChallengeStore {
-  delete(publicKey: PublicKey): void;
-  getById(publicKey: PublicKey): Challenge;
+  pop(publicKey: PublicKey): Challenge;
   save(publicKey: PublicKey, challenge: Challenge): void;
 }
