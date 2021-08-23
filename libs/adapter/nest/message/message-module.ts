3af0e6613fbb6f22407b4a59991637ae02409d90
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-08-22 22:21:59 -0500

    refactor: remove nest logger (#152)

--- libs/adapter/nest/message/message-module.ts
+++ libs/adapter/nest/message/message-module.ts
@@ -1,8 +1,8 @@
-import { DynamicModule, Logger, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { Context, HandlerMap } from '@pl-oss/core';
 import { MessageController } from './message-controller';
 
-@Module({ controllers: [MessageController], providers: [Logger] })
+@Module({ controllers: [MessageController] })
 export class MessageModule {
   static register(context: Context, handlerMap: HandlerMap): DynamicModule {
     const providers = [
