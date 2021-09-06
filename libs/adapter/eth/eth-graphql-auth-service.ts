1cc2e919ab62a7b2371d88de71fdd6504cea074d
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-09-05 23:25:23 -0500

    spacing

--- libs/adapter/eth/eth-graphql-auth-service.ts
+++ libs/adapter/eth/eth-graphql-auth-service.ts
@@ -4,14 +4,14 @@ export class EthGraphQLAuthService {
   constructor(private readonly url: string) {}
 
   async getHexMessage(address: string): Promise<string> {
-    const query = `mutation{ generateAuthMessage(walletId: "${address}") }`;
+    const query = `mutation { generateAuthMessage(walletId: "${address}") }`;
     const response = await axios.post(this.url, { query });
     const unsignedMessageBuffer = Buffer.from(response.data.data.generateAuthMessage, 'utf8');
     return `0x${unsignedMessageBuffer.toString('hex')}`;
   }
 
   async getAccessToken(signedMessage: string, walletId: string): Promise<string> {
-    const query = `mutation{ generateAccessToken(signedMessage: "${signedMessage}" walletId: "${walletId}")}`;
+    const query = `mutation { generateAccessToken(signedMessage: "${signedMessage}" walletId: "${walletId}")}`;
     const response = await axios.post(this.url, { query });
     return response.data.data.generateAccessToken;
   }
