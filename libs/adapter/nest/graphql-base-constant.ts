53ba51deee00b45bddfeafb83e06f1174752776a
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-09-16 01:21:34 -0500

    refactor: graphql base constants

--- libs/adapter/nest/graphql-base-constant.ts
+++ libs/adapter/nest/graphql-base-constant.ts
@@ -1,4 +1,8 @@
+import { Int } from '@nestjs/graphql';
+
 export class GraphQLBaseConstant {
   static DONE = 'done';
+  static RETURN_BOOLEAN = () => Boolean;
+  static RETURN_INT = () => Int;
   static RETURN_STRING = () => String;
 }
