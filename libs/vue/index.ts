fcb2e9d35e89189360ff5a088766a5ff7ca016b1
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-09-19 23:17:43 -0500

    feat: add DomHelper

--- libs/vue/index.ts
+++ libs/vue/index.ts
@@ -3,4 +3,5 @@ export * from './guard/web3-guard';
 export * from './plugin/auth0-plugin';
 export * from './plugin/web3-plugin';
 export * from './plugin/pl-components-plugin';
+export * from './service/dom-helper';
 export * from './service/eth-graphql-auth-service';
