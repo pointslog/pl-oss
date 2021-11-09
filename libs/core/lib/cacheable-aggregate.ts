fd9a54daa52ae884b71883addcfd060f020c285c
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-11-09 16:53:06 -0600

    feat: add cacheable aggregate

---
+++ libs/core/lib/cacheable-aggregate.ts
@@ -0,0 +1,25 @@
+import { Aggregate } from './aggregate';
+import { EventStore } from './event-store';
+
+export abstract class CacheableAggregate extends Aggregate {
+  private static cache: Record<string, CacheableAggregate> = {}
+
+  async commit(eventStore: EventStore): Promise<this> {
+    try {
+      const result = await super.commit(eventStore);
+      return result;
+    } catch (e) {
+      delete CacheableAggregate.cache[this.id];
+      throw (e);
+    }
+  }
+
+  async load(eventStore: EventStore, skipCache = false): Promise<this> {
+    const cached = CacheableAggregate.cache[this.id];
+    if (!skipCache && cached) return cached as this;
+
+    const result = await super.load(eventStore);
+    CacheableAggregate.cache[this.id] = result;
+    return result;
+  }
+}
