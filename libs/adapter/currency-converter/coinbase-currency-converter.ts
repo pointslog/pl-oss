1f5ba365285931a520916ef9f319121bd20a01e1
Ronak Mistry <ronakmistry36@gmail.com>
2022-02-08 20:18:53 -0600

    Coinbase response type

--- libs/adapter/currency-converter/coinbase-currency-converter.ts
+++ libs/adapter/currency-converter/coinbase-currency-converter.ts
@@ -1,12 +1,18 @@
 import { CurrencyConversionFailedException, CurrencyConverter } from '@pl-oss/core';
 import axios from 'axios';
 
+type CoinbaseResponse = {
+  data: {
+    amount: string;
+  };
+}
+
 export class CoinbaseCurrencyConverter implements CurrencyConverter {
   private readonly baseUrl = 'https://api.coinbase.com/v2';
   private readonly unitCents = 100;
 
   async fromEthToUsd(amount: number): Promise<number> {
-    const response = await axios.get<{ data: { amount: string } }>(`${this.baseUrl}/prices/ETH-USD/spot`);
+    const response = await axios.get<CoinbaseResponse>(`${this.baseUrl}/prices/ETH-USD/spot`);
     if (response.status !== 200) throw new CurrencyConversionFailedException(amount, 'eth', 'usd');
     return (amount / 1_000_000_000) * Number.parseFloat(response.data.data.amount);
   }
