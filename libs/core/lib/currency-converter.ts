941ffd06e3bd62299f49a4291a3a2ecacd5f18ee
Ronak Mistry <ronakmistry36@gmail.com>
2022-02-08 21:02:03 -0600

    More methods added

--- libs/core/lib/currency-converter.ts
+++ libs/core/lib/currency-converter.ts
@@ -1,4 +1,6 @@
 export interface CurrencyConverter {
+  fromGweiToUsd(amount: number): Promise<number>;
+  fromGweiToEth(amount: number): Promise<number>;
   fromEthToUsd(amount: number): Promise<number>;
-  fromUsdToCents(amount: number): number;
+  fromUsdToCents(amount: number): Promise<number>;
 }
