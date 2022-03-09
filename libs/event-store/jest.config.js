ed97401502764b65558dc617f0535bdaf079077e
Ronak Mistry <ronakmistry36@gmail.com>
2022-03-08 22:07:57 -0600

    event store first commit

---
+++ libs/event-store/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'event-store',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/event-store',
+};
