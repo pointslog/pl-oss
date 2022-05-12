5c3c6e516e6fcc09706ab82541a0afb97304085c
Ronak Mistry <ronakmistry36@gmail.com>
2022-05-12 14:26:54 -0500

    WIP

--- libs/mongodb/file-store/mongo-db-file-store.ts
+++ libs/mongodb/file-store/mongo-db-file-store.ts
@@ -1,12 +1,12 @@
 import { FileStore, FileEntity, IdShouldBeUniqueException } from '@pl-oss/core';
-import { Collection } from 'mongodb';
+import { Collection, OptionalUnlessRequiredId } from 'mongodb';
 
 export class MongoDBFileStore implements FileStore {
   constructor(private readonly collection: Collection) {}
 
   async save(fileEntity: FileEntity): Promise<void> {
     try {
-      const withMongoId = { ...fileEntity, _id: fileEntity.id };
+      const withMongoId = { ...fileEntity, _id: fileEntity.id } as OptionalUnlessRequiredId<any>;
       await this.collection.insertOne(withMongoId);
     } catch (e) {
       const DUPLICATE_KEY_ERROR_CODE = 11000;
